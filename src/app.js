42403a54bd1ad43b1a43a1dad3b35640ff3c6001
khokha1553 <khokha1553@gmail.com>
2026-02-09 16:52:38 +0200

    edit app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
+import express from "express";
+import notificationRoutes from "./src/modules/notifications/notification.routes.js";
+
 const app = express();
+
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Notifications API is running");
+});
+
 app.use("/notifications", notificationRoutes);
+
 export default app;
